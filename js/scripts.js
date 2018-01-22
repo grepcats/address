9bfc1c7164d0388b168a493db43af1ae1743c7b8
Hannah Lee and Kayla Ondracek <hannah@>
2018-01-22 08:42:31 -0800

    add more form fields for address and output

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,20 @@
 //business logic
-function Contact(first, last) {
+function Contact(first, last, street, city, state, zip) {
     this.firstName = first;
     this.lastName = last;
+    this.streetText = street;
+    this.cityText = city;
+    this.stateAbbr = state;
+    this.zipCode = zip;
 }
 
+Contact.prototype.fullName = function() {
+  return this.firstName + " " + this.lastName;
+}
+
+Contact.prototype.cityState = function() {
+  return this.cityText + " " + this.stateAbbr;
+}
 
 
 //user interface logic
@@ -13,20 +24,32 @@ $(document).ready(function() {
 
     var inputtedFirstName = $("input#new-first-name").val();
     var inputtedLastName = $("input#new-last-name").val();
+    var inputtedStreet = $("input#new-street").val();
+    var inputtedCity = $("input#new-city").val();
+    var inputtedState = $("input#new-state").val();
+    var inputtedZip = $("input#new-zip").val();
 
-    var newContact = new Contact(inputtedFirstName, inputtedLastName);
 
-    $("ul#contacts").append("<li><span class='contact'>" + newContact.firstName + "</span></li>");
+    var newContact = new Contact(inputtedFirstName, inputtedLastName, inputtedStreet, inputtedCity, inputtedState, inputtedZip);
+
+    $("ul#contacts").append("<li><span class='contact'>" + newContact.fullName() + "</span></li>");
 
     $(".contact").last().click(function() {
       $("#show-contact").show();
       $("#show-contact h2").text(newContact.firstName);
       $(".first-name").text(newContact.firstName);
       $(".last-name").text(newContact.lastName);
+      $(".street").text(newContact.streetText);
+      $(".cityState").text(newContact.cityState());
+      $(".zip").text(newContact.zipCode);
     });
 
     $("input#new-first-name").val("");
     $("input#new-last-name").val("");
+    $("input#new-street").val("");
+    $("input#new-city").val("");
+    $("input#new-state").val("");
+    $("input#new-zip").val("");
   });
 
 });
